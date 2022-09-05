fa2437a49af460d0db9ec5a5c0b63cc8b4797a39
Nicolas Dolan <nicolas.dolan@ymail.com>
2022-09-05 16:05:45 +0100

    fixed styling

--- src/bar/BarChartWithFilters.vue
+++ src/bar/BarChartWithFilters.vue
@@ -46,7 +46,7 @@
                         :yFormat="formatValueFunction"
                         :show-errors="showRangesInTooltips"
                         style="width: 100%; height: 100%;"></bar-chart-with-errors>
-                <div v-if="showNoDataMessage" id="noDataMessage" class="px-3 py-2" style="width: 60%; position: absolute; border: 1px solid #adb5bd; bottom: 50%; left: 25%; background: #fff; margin: auto;">
+                <div v-if="showNoDataMessage" id="noDataMessage" class="px-3 py-2 noDataMessage">
                     <span class="lead">
                         <strong>{{ noDataMessage }}</strong>
                     </span>
