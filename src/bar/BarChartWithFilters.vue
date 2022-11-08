2110bba6e50742b2b19ac30c8d4027929aa50fc7
Lekan Anifowoshe <anney.lake@gmail.com>
2022-11-08 14:27:57 +0000

    change to viewport to 100vh

--- src/bar/BarChartWithFilters.vue
+++ src/bar/BarChartWithFilters.vue
@@ -45,7 +45,7 @@
                         :yLabel="indicatorLabel"
                         :yFormat="formatValueFunction"
                         :show-errors="showRangesInTooltips"
-                        style="min-width: 90vh; min-height: 90vh; position: relative"></bar-chart-with-errors>
+                        style="width: 100vh; height: 100vh; position: relative"></bar-chart-with-errors>
                 <div v-if="showNoDataMessage" id="noDataMessage" class="px-3 py-2 noDataMessage">
                     <span class="lead">
                         <strong>{{ noDataMessage }}</strong>
